e1da7255970fee9e26b1d8b7782b03614cc1ccc3
shota-ishi73 <1473shota@gmail.com>
2021-05-02 15:28:44 +0900

    user管理機能実装４

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,11 @@ class User < ApplicationRecord
   validates :password, length: {minimum:6},format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, message: "は英数字混合にしてください"}
   validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "must include @" }
   with_options presence: true do
-    validates :nickname, presence: {message:"can't be blank"} 
-    validates :first_name, presence: {message: "can't be blank"},format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は漢字・平仮名・カタカナにしてください"}
-    validates :last_name, presence: {message: "can't be blank"},format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は漢字・平仮名・カタカナにしてください"}
-    validates :first_name_kana, presence: {message: "can't be blank"},format: {with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナにしてください"}
-    validates :last_name_kana, presence: {message: "can't be blank"},format: {with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナにしてください"}
-    validates :birth_day, presence: {message: "can't be blank"}
+    validates :nickname
+    validates :first_name,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は漢字・平仮名・カタカナにしてください"}
+    validates :last_name,format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は漢字・平仮名・カタカナにしてください"}
+    validates :first_name_kana,format: {with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナにしてください"}
+    validates :last_name_kana,format: {with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナにしてください"}
+    validates :birth_day
   end
 end
