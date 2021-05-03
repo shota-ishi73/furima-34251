a302a7684de3d310d9e3a4628cd7722c29950d26
shota-ishi73 <1473shota@gmail.com>
2021-05-03 20:47:46 +0900

    商品詳細ex2
    軽微な修正

--- app/models/delivery_day.rb
+++ app/models/delivery_day.rb
@@ -1,9 +1,9 @@
 class DeliveryDay < ActiveHash::Base
   self.data = [
     { id: 1, name: '---'},
-    { id: 2, name: '１〜２で発送'},
-    { id: 3, name: '３〜４で発送'},
-    { id: 4, name: '５〜６で発送'}
+    { id: 2, name: '１〜２日で発送'},
+    { id: 3, name: '３〜４日で発送'},
+    { id: 4, name: '５〜６日で発送'}
   ]
 
   include ActiveHash::Associations
