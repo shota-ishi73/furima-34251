0bd0897c52db544944b9b3f0ecb33d54e934fdb1
shota-ishi73 <1473shota@gmail.com>
2021-05-03 18:32:32 +0900

    商品詳細

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,11 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+    @user = User.find_by(id: @item.user_id)
+  end
+
   private
 
   def item_params
