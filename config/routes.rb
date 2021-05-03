b11b02bd05a33ad910474d793e97ccf7eef30f82
shota-ishi73 <1473shota@gmail.com>
2021-05-03 21:39:01 +0900

    商品情報編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'items/index'
   root to: 'items#index'
 
-  resources :items, only: [:index, :new, :create, :show] do
+  resources :items, only: [:index, :new, :create, :show, :edit, :update] do
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
